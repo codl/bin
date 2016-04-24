da402c1df9017bc92c80a7729081beb4a4491058
codl (Corentin Delcourt) <codl@codl.fr>
2016-04-24 08:22:30 +0200

    yt: add faststart atom so youtube can start encoding as soon as possible

--- yt.sh
+++ yt.sh
@@ -4,11 +4,15 @@ COVER="$2"
 
 duration=$(ffprobe -v 0 -show_entries format=duration -of flat "$FILE" | sed 's/format.duration="\([0-9.]\+\)"/\1/')
 
+COVER_PARAMS='-loop 1 -r 6'
+if egrep ".gif$" <<< "$COVER"; then
+    COVER_PARAMS='-ignore_loop 0'
+fi
+
 ffmpeg \
-    -loop 1 -r 6 -i "$COVER"\
+    $COVER_PARAMS -i "$COVER"\
     -i "$FILE"\
-    -vf scale=-1:720 -c:v libx264\
+    -vf scale=-1:720 -c:v libx264 -movflags +faststart\
     -c:a aac -b:a 192k\
     -t "$duration"\
     -y "/tmp/$(basename "$FILE").m4v"
-
