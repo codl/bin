4cf0a209cdec32f03e8ec2818ead65f1ff11b0c9
codl <codl@codl.fr>
2017-05-20 13:11:15 +0200

    backup: wait up to 3 minutes for the repo lock to be released

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-borg create -vsp -C zlib "::{hostname}-home-{now:%Y-%m-%dT%H:%M}" /home/codl
-borg prune -vs --list -H 24 -d 31 -w 8 -m 24 -y 999 -P '{hostname}'
+borg create --lock-wait 180 -vsp -C zlib "::{hostname}-home-{now:%Y-%m-%dT%H:%M}" /home/codl
+borg prune --lock-wait 180 -vs --list -H 24 -d 31 -w 8 -m 24 -y 999 -P '{hostname}'
