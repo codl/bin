dbbfad221b6645ce8b464baf5f45dba95a1244b8
codl <codl@codl.fr>
2017-06-08 09:21:15 +0200

    backup.sh: prune more aggressively

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 borg create --lock-wait 180 -vsp -C zlib "::{hostname}-home-{now:%Y-%m-%dT%H:%M}" /home/codl
-borg prune --lock-wait 180 -vs --list -H 24 -d 31 -w 8 -m 24 -y 999 -P '{hostname}'
+borg prune --lock-wait 180 -vs --list -H 12 -d 6 -w 3 -m 23 -y 999 -P '{hostname}'
