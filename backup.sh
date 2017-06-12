fa8a8fa080ef9d8fe724ad86380d255c0c1f3bee
codl <codl@codl.fr>
2017-06-12 12:47:22 +0200

    backup: exclude cachedirs, bump prune limits up again

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-borg create --lock-wait 180 -vsp -C zlib "::{hostname}-home-{now:%Y-%m-%dT%H:%M}" /home/codl
-borg prune --lock-wait 180 -vs --list -H 12 -d 6 -w 3 -m 23 -y 999 -P '{hostname}'
+borg create --lock-wait 180 -vsp -C zlib --exclude-caches "::{hostname}-home-{now:%Y-%m-%dT%H:%M}" /home/codl
+borg prune --lock-wait 180 -vs --list -H 23 -d 30 -m 23 -y 999 -P '{hostname}'
